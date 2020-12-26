272ce12231c0b3a09530fa973ed1e47ed8eca982
pau <pllinas@cifpfbmoll.eu>
2020-12-26 03:06:11 +0100

    feat: :sparkles: All kind of integres, null combinations and spares are working, not passing strikes and mixed tests

--- bowling.py
+++ bowling.py
@@ -2,16 +2,25 @@ def bowling_game(frames):
     frames = str(frames)
     numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     strike = False
+    spare = False
     punctutation = 0
+    throw_counter = 0
     strike_counter = 1
     i = 0
     while i < len(frames):
         if frames[i] in numbers:
-            if strike == True:
+            if strike == True and throw_counter < 20:
                 punctutation += int(frames[i]) * 2
-                strike_counter += 1
+                """
+                if strike_counter > 2:
+                    strike = False
+                    """
+            elif spare == True and throw_counter < 20:
+                punctutation += int(frames[i]) * 2
+                spare = False
             else:
                 punctutation += int(frames[i])
+            throw_counter += 1
             strike = False
             strike_counter = 0
             i += 1
@@ -22,16 +31,40 @@ def bowling_game(frames):
             else:
                 punctutation += 10
 # If the strike counter is not equal to the number 3 and the throw we are looking is below the number 9 we add one to the strike counter, because we want to remember that we have done an strike for the next throws. In case we are in the last throws, the number 9, 10 or 11 we want to rest one to the strike counter because we can't still multiplying the next throws because we can't do anymore after the 12 throw
-            if strike_counter != 3 and i < 9:
+            if strike_counter != 3 and throw_counter < 20:
                 strike_counter += 1
-            elif i == 9 or i == 10 or i == 11:
+            elif i > 19:
                 strike_counter -= 1
 # Finally we want to add plus one to the i just to keep moving in the string of bowling frames and we want to set up the variable of strikes to True because we just have done a strike in the game
+            throw_counter += 2
             i += 1
             strike = True
+# Conditional for the wasted throws during the bowling game
+        elif frames[i] == '-':
+            i += 1
+            throw_counter += 1
+            continue
+# Conditional in case the throw is a spare
+        elif frames[i] == '/':
+            punctutation += 10 - int(frames[i - 1])
+            spare = True
+            throw_counter += 1
+            i += 1
     return punctutation
 
 
 if __name__ == "__main__":
+    #!INTEGRES
     #assert bowling_game(12345123451234512345) == 60
-    assert bowling_game('XXXXXXXXXXXX') == 300
+    #!NULL
+    #assert bowling_game('9-9-9-9-9-9-9-9-9-9-') == 90
+    #!SPARES
+    #assert bowling_game('5/5/5/5/5/5/5/5/5/5/5') == 150
+    #assert bowling_game('5/324/5/343152424152') == 82
+    #assert bowling_game('3/4/5/3/1/421/8/2/6/7') == 136
+    #!STRIKES
+    #assert bowling_game('XXXXXXXXXXXX') == 300
+    assert bowling_game('42X4225X5224524536') == 87
+    #!MIXED
+
+    #!assert bowling_game('625/6353X436/2441-5') == 93
