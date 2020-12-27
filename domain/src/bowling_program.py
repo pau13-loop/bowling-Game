c5c4de5cd5177656c73af3ec18a04120b5990252
pau <pllinas@cifpfbmoll.eu>
2020-12-27 01:52:49 +0100

    perf: :zap: Finally our code is completely functional and working, it pass all the test cases

--- domain/src/bowling_program.py
+++ domain/src/bowling_program.py
@@ -5,10 +5,8 @@ def bowling_game(frames):
     punctutation = 0
     throw_counter = 0
     i = 0
-    while i < len(frames):
+    while throw_counter < 20:
         if frames[i] in numbers:
-            # if strike == True and throw_counter < 20:
-            # punctutation += int(frames[i]) * bonus_strike_punctutation
             if spare == True and throw_counter < 20:
                 punctutation += int(frames[i]) * 2
                 spare = False
@@ -18,59 +16,55 @@ def bowling_game(frames):
             i += 1
 # In case the throw has been a strike we go inside the "elif" conditional. If the strike variable is set to True we add to the punctutation 10 pins plus the strike counter that keeps the count of how many strikes we have done before this throw, if strike is not set to True means that we didn't have done an strike before and we just add to the punctutation the 10 pins we shoot down
         elif frames[i] == 'X':
-            punctutation += 10
+            if spare == True:
+                punctutation += 10 * 2
+                spare = False
+            else:
+                punctutation += 10
             i += 1
             throw_counter += 2
             # INT - (INT, SPARE, NULL)
-            if frames[i] in numbers:
-                punctutation += int(frames[i])
-                # - INT
-                if frames[i+1] in numbers:
-                    punctutation += int(frames[i+1])
-                # - SPARE
-                elif frames[i+1] == '/':
-                    punctutation += 10 - int(frames[i])
-                # - NULL
-                elif frames[i+1] == '-':
-                    continue
-            # NULL - (INT, SPARE, NULL)
-            elif frames[i] == '-':
-                # - INT
-                if frames[i+1] in numbers:
-                    punctutation += int(frames[i+1])
-                # - SPARE
-                elif frames[i+1] == '/':
-                    if frames[i] == '-':
-                        punctutation += 10
-                    else:
+            if throw_counter < 20:
+                if frames[i] in numbers:
+                    punctutation += int(frames[i])
+                    # - INT
+                    if frames[i+1] in numbers:
+                        punctutation += int(frames[i+1])
+                    # - SPARE
+                    elif frames[i+1] == '/':
                         punctutation += 10 - int(frames[i])
-                # - NULL
-                elif frames[i+1] == '-':
-                    continue
-            # STRIKE - (INT, STRIKE, NULL)
-            elif frames[i] == 'X':
-                punctutation += 10
-                # - INT
-                if frames[i+1] in numbers:
-                    punctutation += int(frames[i+1])
-                # - STRIKE
-                elif frames[i+1] == 'X':
+                    # - NULL
+                    elif frames[i+1] == '-':
+                        continue
+                # NULL - (INT, SPARE, NULL)
+                elif frames[i] == '-':
+                    # - INT
+                    if frames[i+1] in numbers:
+                        punctutation += int(frames[i+1])
+                    # - SPARE
+                    elif frames[i+1] == '/':
+                        if frames[i] == '-':
+                            punctutation += 10
+                        else:
+                            punctutation += 10 - int(frames[i])
+                    # - NULL
+                    elif frames[i+1] == '-':
+                        continue
+                # STRIKE - (INT, STRIKE, NULL)
+                elif frames[i] == 'X':
                     punctutation += 10
-                # - NULL
-                elif frames[i+1] == '-':
-                    continue
-            """
-        if strike == True and throw_counter < 20:
-            punctutation += 10 * bonus_strike_punctutation
-        else:
-            punctutation += 10
-# If the strike counter is not equal to the number 3 and the throw we are looking is below the number 9 we add one to the strike counter, because we want to remember that we have done an strike for the next throws. In case we are in the last throws, the number 9, 10 or 11 we want to rest one to the strike counter because we can't still multiplying the next throws because we can't do anymore after the 12 throw
-# Finally we want to add plus one to the i just to keep moving in the string of bowling frames and we want to set up the variable of strikes to True because we just have done a strike in the game
-        bonus_strike_punctutation += 1
-            """
-        # strike = True
+                    # - INT
+                    if frames[i+1] in numbers:
+                        punctutation += int(frames[i+1])
+                    # - STRIKE
+                    elif frames[i+1] == 'X':
+                        punctutation += 10
+                    # - NULL
+                    elif frames[i+1] == '-':
+                        continue
 # Conditional for the wasted throws during the bowling game
         elif frames[i] == '-':
+            spare = False
             i += 1
             throw_counter += 1
             continue
@@ -83,4 +77,14 @@ def bowling_game(frames):
             spare = True
             throw_counter += 1
             i += 1
+    while i < len(frames):
+        if frames[i] in numbers:
+            punctutation += int(frames[i])
+        elif frames[i] == 'X':
+            punctutation += 10
+        elif frames[i] == '/':
+            punctutation += 10 - int(frames[i - 1])
+        else:
+            continue
+        i += 1
     return punctutation
