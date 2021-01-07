812b3d41e895bfa1fe5ae09dd6747e9f391c734a
pau <pllinas@cifpfbmoll.eu>
2021-01-07 23:16:24 +0100

    fix: :bug: The bowling program finally is completely functional and operative

--- domain/src/main.py
+++ domain/src/main.py
@@ -1,133 +1,94 @@
-i = 0
-throw_counter = 0
-numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-
 class Bowling():
 
     def __init__(self, frames):
         self.frames = frames
+        self.i = 0
+        self.throw_counter = 0
+        self.numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
     def strike(self):
 
-        global i
-        global throw_counter
-        global numbers
-
-        points = 0
-
-        points += 10
-        throw_counter += 2
-        i += 1
+        points = 10
+        self.throw_counter += 2
+        self.i += 1
 
         # INT - (INT, SPARE)
-        if throw_counter < 20 and self.frames[i] in numbers:
-            points += int(self.frames[i])
+        if self.throw_counter < 20 and self.frames[self.i] in self.numbers:
+            points += int(self.frames[self.i])
             # - INT
-            if self.frames[i+1] in numbers:
-                points += int(self.frames[i+1])
+            if self.frames[self.i+1] in self.numbers:
+                points += int(self.frames[self.i+1])
             # - SPARE
-            elif self.frames[i+1] == '/':
-                points += 10 - int(self.frames[i])
-
-        # NULL - (INT, SPARE)
-        elif self.frames[i] == '-':
-            # - INT
-            if self.frames[i+1] in numbers:
-                points += int(self.frames[i+1])
-            # - SPARE
-            elif self.frames[i+1] == '/':
-                points += 10
+            elif self.frames[self.i+1] == '/':
+                points += 10 - int(self.frames[self.i])
 
         # STRIKE - (INT, STRIKE)
-        elif throw_counter < 20 and self.frames[i] == 'X':
+        elif self.throw_counter < 20 and self.frames[self.i] == 'X':
             points += 10
             # - INT
-            if self.frames[i+1] in numbers:
-                points += int(self.frames[i+1])
+            if self.frames[self.i+1] in self.numbers:
+                points += int(self.frames[self.i+1])
             # - STRIKE
-            elif self.frames[i+1] == 'X':
+            elif self.frames[self.i+1] == 'X':
                 points += 10
 
         return points
 
     def spare(self):
 
-        global i
-        global throw_counter
-        global numbers
-
         points = 0
 
-        if self.frames[i - 1] in numbers:
-            points += 10 - int(self.frames[i - 1])
+        if self.frames[self.i - 1] in self.numbers:
+            points += 10 - int(self.frames[self.i - 1])
         else:
             points += 10
 
-        throw_counter += 1
-        i += 1
+        self.throw_counter += 1
+        self.i += 1
 
         # bonus score for the spare
-        if throw_counter < 20:
-            if self.frames[i] == 'X':
+        if self.throw_counter < 20:
+            if self.frames[self.i] == 'X':
                 points += 10
-            elif self.frames[i] in numbers:
-                points += int(self.frames[i])
+            elif self.frames[self.i] in self.numbers:
+                points += int(self.frames[self.i])
 
         return points
 
-    def null(self):
-
-        global i
-        global throw_counter
-
-        i += 1
-        throw_counter += 1
-
     def integer(self):
 
-        global throw_counter
-        global i
-
-        points = int(self.frames[i])
-        i += 1
-        throw_counter += 1
+        points = int(self.frames[self.i])
+        self.i += 1
+        self.throw_counter += 1
 
         return points
 
     def extra_frames(self):
 
-        global i
-        global numbers
-
         points = 0
 
-        while i < len(self.frames):
-            if self.frames[i] in numbers:
-                points += int(self.frames[i])
-            elif self.frames[i] == 'X':
+        while self.i < len(self.frames):
+            if self.frames[self.i] in self.numbers:
+                points += int(self.frames[self.i])
+            elif self.frames[self.i] == 'X':
                 points += 10
-            elif self.frames[i] == '/':
-                points += 10 - int(self.frames[i - 1])
-            i += 1
+            elif self.frames[self.i] == '/':
+                points += 10 - int(self.frames[self.i - 1])
+            self.i += 1
 
         return points
 
     def bowling_game(self):
 
-        global i
-        global throw_counter
-        global numbers
-
         score = 0
 
-        while throw_counter < 20:
-            if self.frames[i] == 'X':
+        self.frames = self.frames.replace('-', '0')
+
+        while self.throw_counter < 20:
+            if self.frames[self.i] == 'X':
                 score += self.strike()
-            elif self.frames[i] == '/':
+            elif self.frames[self.i] == '/':
                 score += self.spare()
-            elif self.frames[i] == '-':
-                self.null()
             else:
                 score += self.integer()
         score += self.extra_frames()
